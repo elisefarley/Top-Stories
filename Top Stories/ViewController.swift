4165180015c06938be15a065de26962d30e3baed
Elise Farley <efarley@bsd220.org>
2020-01-28 10:42:49 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Elise Farley on 1/28/20.
+//  Copyright © 2020 Elise Farley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
